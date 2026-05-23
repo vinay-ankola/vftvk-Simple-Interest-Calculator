39e42ffc8e6cf49fb39cb4e6671492f3b3fbd1fc
Vinay Ankola <vinay.ankola@gmail.com>
2026-05-22 22:44:06 -0400

    Modified script.js style.css

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
+
+    if (principal <=0) {
+        alert("Please enter a positive number!");
+        document.getElementById("principal").focus();
+    } else {
+        result.innerHTML = "If you deposit $" + principal + ",\<br> at an interest rate of " + rate + "%.\<br> Your interest will be $" + interest + ".\<br>You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br>in the year " + year + "\<br>"
+    }
     
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
